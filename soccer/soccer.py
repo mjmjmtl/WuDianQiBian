4f61d708c3bbc75b2a2c157681dc20650fc965c7
5Points7Edges <5points7edges@gmail.com>
2022-12-15 15:04:23 -0800

    polish

--- soccer/soccer.py
+++ soccer/soccer.py
@@ -6,14 +6,15 @@ import networkx as nx
 import trimesh
 from scipy.spatial.transform import Rotation
 
-def get_soccer_points(subdivisions):
-    sphere: trimesh.Trimesh = trimesh.creation.icosphere(subdivisions=subdivisions)
 
+def get_soccer_points(subdivisions):
+    # initialization
+    sphere = trimesh.creation.icosphere(subdivisions=subdivisions)
+    graph = sphere.vertex_adjacency_graph
     key_point_distance = 1 << subdivisions
+    is_black = np.full(len(sphere.vertices), False)
 
     # black pieces on soccer ball
-    is_black = np.full(len(sphere.vertices), False)
-    graph: nx.Graph = sphere.vertex_adjacency_graph
     for u in range(12):
         for _, v in nx.bfs_edges(graph, u, depth_limit=key_point_distance // 3):
             is_black[v] = True
@@ -42,9 +43,8 @@ R2 = 2
 K2 = 20
 K1 = screen_size * K2 * 3 / (8 * (R1 + R2))
 
-
-
 black_points, white_points = get_soccer_points(6)
+
 
 def render_frame(A: float, B: float) -> np.ndarray:
     """
@@ -59,44 +59,41 @@ def render_frame(A: float, B: float) -> np.ndarray:
     output = np.full((screen_size, screen_size), " ")  # (40, 40)
 
     radius = 3.5
+
     def _draw_points(points, is_black):
         points = points * radius @ rotation_matrix.T
         # points = points * radius
         # points = rotation_matrix.apply(points)
-        for x,y,z in points:
+        for x, y, z in points:
             # x *= radius
             # y *= radius
             # z *= radius
             # x, z = x * cos_A - z * sin_A, z * cos_A + x * sin_A
             if z > 0:
                 continue
-            ooz = 1 / (z+K2)   # "one over z"
+            ooz = 1 / (z + K2)  # "one over z"
             xp = int(screen_size / 2 + K1 * ooz * x)
             yp = int(screen_size / 2 - K1 * ooz * y)
             if is_black:
                 output[yp, xp] = ' '
                 continue
-            l = np.dot((x,y,z), (0, 1, -1)) / 3
+            l = np.dot((x, y, z), (0, 1, -1)) / 3
             # if l < 0:
             #     continue
-            luminance_index = int(l*8)
-            luminance_index = min(luminance_index, len(illumination)-1)
+            luminance_index = int(l * 8)
+            luminance_index = min(luminance_index, len(illumination) - 1)
 
             output[yp, xp] = illumination[luminance_index]
-
 
     _draw_points(white_points, False)
     _draw_points(black_points, True)
 
-
     return output
-
 
 
 def pprint(array: np.ndarray) -> None:
     """Pretty print the frame."""
     print(*[''.join(np.repeat(row, 2)) for row in array], sep="\n")
-
 
 
 colorama.init()
